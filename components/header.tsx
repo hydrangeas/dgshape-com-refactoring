eb706b1932fe91af4df675f4abc77bcbab977452
Akira Otaka <hydrangeas.project@gmail.com>
2023-09-18 03:54:34 +0000

    header/footerをコンポーネント化

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+export const Header: FunctionComponent = () => {
+  return (
+    <div className="nav">
+      <header className="_title">DGSHAPE</header>
+    </div>
+  );
+};
